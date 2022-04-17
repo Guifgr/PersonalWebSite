fd8ae9b651b78842e98b593f7e81e81209701070
Guifgr <guifgr@hotmail.com>
2022-04-17 13:19:22 -0300

    Update about me and fix css

--- src/components/microComponents/Icons.js
+++ src/components/microComponents/Icons.js
@@ -1,49 +1,63 @@
-import React from "react"
-import InstaLogo from "../../img/instagram.webp"
-import LinkedInLogo from "../../img/linkedin.webp"
-import gitLogo from "../../img/git.webp"
+import React from "react";
+import InstaLogo from "../../img/instagram.webp";
+import LinkedInLogo from "../../img/linkedin.webp";
+import gitLogo from "../../img/git.webp";
 
-function Icons(){
-    return(
+function Icons() {
+    return (
         <div className="icon" style={Icon}>
             <div className="icons" style={iconDiv}>
                 <a href="https://github.com/Guifgr">
-                    <img loading="lazy" alt="git" className="imgIcon" style={iconImg} src={gitLogo}></img>
+                    <img
+                        alt="git"
+                        className="imgIcon"
+                        style={iconImg}
+                        src={gitLogo}
+                    ></img>
                 </a>
             </div>
-        
+
             <div className="icons" style={iconDiv}>
                 <a href="https://www.linkedin.com/in/guifgr/">
-                    <img loading="lazy" alt="linkedin" className="imgIcon" style={iconImg} src={LinkedInLogo}></img>
+                    <img
+                        alt="linkedin"
+                        className="imgIcon"
+                        style={iconImg}
+                        src={LinkedInLogo}
+                    ></img>
                 </a>
             </div>
 
             <div className="icons" style={iconDiv}>
                 <a href="https://www.instagram.com/guifgr/">
-                    <img loading="lazy" alt="insta" className="imgIcon" style={iconImg} src={InstaLogo}></img>
+                    <img
+                        alt="insta"
+                        className="imgIcon"
+                        style={iconImg}
+                        src={InstaLogo}
+                    ></img>
                 </a>
             </div>
-
         </div>
     );
 }
 
-const Icon={
-    width:"100%",
+const Icon = {
+    width: "100%",
     display: "flex",
     justifyContent: "center",
     height: "20vh",
     marginTop: "5vh",
-    marginBottom: "10vh"
-}
+    marginBottom: "10vh",
+};
 
-const iconDiv={
-    display:"block",
-    height:"100%",
-    width:"20%",
-    margin: "10%"
-}
-const iconImg={
-    width:"80%"
-}
+const iconDiv = {
+    display: "block",
+    height: "100%",
+    width: "20%",
+    margin: "10%",
+};
+const iconImg = {
+    width: "80%",
+};
 export default Icons;
--- src/components/microComponents/Text.js
+++ src/components/microComponents/Text.js
@@ -1,31 +1,57 @@
-import React from "react"
-import Icons from "./Icons"
+import React from "react";
+import Icons from "./Icons";
 
+function Text() {
+    return (
+        <div
+            className="half"
+            style={{ float: "left", marginLeft: "15%", width: "34.5%" }}
+        >
+            <p style={{ marginTop: "0", marginBottom: "1vh" }}>
+                Me chamo Guilherme atualmente estudo na Universidade de Mogi das
+                Cruzes, amo tecnologia, trabalho como{" "}
+                <a style={{ color: "black" }} href="https://dev.to/guifgr">
+                    <b>Desenvolvedor Back-end</b>
+                </a>{" "}
+                e <b>Mentor</b> do Meta League na{" "}
+                <a style={{ color: "black" }} href="https://www.meta.com.br/">
+                    <b>META</b>
+                </a>
+                , um dos meus maiores hobbies é basquete e automação.
+            </p>
 
-function Text(){
-    return(
-        <div className="half" style={{float:"left",  marginLeft:"15%", width:"34.5%"}}>
-            
-                <p style={{marginTop:"0", marginBottom:"1vh"}}>Me chamo Guilherme atualmente estudo na Universidade de Mogi das Cruzes,
-                     amo tecnologia, trabalho como <a style={{color:"black"}} href="https://dev.to/guifgr">Dev Back-end</a> na <a style={{color:"black"}} href="https://wlssistemas.com.br/">WLS Sistemas</a>, um dos meus maiores hobbies é basquete e automação.
-                </p>
-            
-                <p style={{marginBottom:"1vh"}}>Estudo <b>Sistemas de Informação</b> e passei por <b>Ciência da Computação</b>, adoro estudar programação e quando me sinto 
-                desmotivado sempre penso em como eu poderia solucionar algum problema do dia a dia.
-                </p>
-            
-                <p style={{marginBottom:"1vh"}}>Atualmente tenho alguns sites no ar como o <a style={{color:"black"}} href="https://chalestriunfo.com.br/">Chalés Triunfo</a> e 
-                muitos outros projetos no <a style={{color:"black"}} href="https://github.com/Guifgr">GitHub</a>.
-                </p>
-                
-                <p style={{marginBottom:"1vh"}}>Adoro lidar com tecnologia seja hardware ou software, desde cedo desmontava tudo que era eletrônico e até hoje gosto do 
-                handmade e sonho em morar em uma casa automatizada, pois a vida é um projeto DIY!
-                </p>
-                
-                <Icons />
-                 
-            </div>
-    )
+            <p style={{ marginBottom: "1vh" }}>
+                Estudo <b>Sistemas de Informação</b> e passei por{" "}
+                <b>Ciência da Computação</b>, adoro estudar programação e quando
+                me sinto desmotivado sempre penso em como eu poderia solucionar
+                algum problema do dia a dia.
+            </p>
+
+            <p style={{ marginBottom: "1vh" }}>
+                Atualmente tenho alguns sites no ar como o{" "}
+                <a
+                    style={{ color: "black" }}
+                    href="https://chalestriunfo.com.br/"
+                >
+                    Chalés Triunfo
+                </a>{" "}
+                e muitos outros projetos no{" "}
+                <a style={{ color: "black" }} href="https://github.com/Guifgr">
+                    GitHub
+                </a>
+                .
+            </p>
+
+            <p style={{ marginBottom: "1vh" }}>
+                Adoro lidar com tecnologia seja hardware ou software, desde cedo
+                desmontava tudo que era eletrônico e até hoje gosto do handmade
+                e sonho em morar em uma casa automatizada, pois a vida é um
+                projeto DIY!
+            </p>
+
+            <Icons />
+        </div>
+    );
 }
 
 export default Text;
